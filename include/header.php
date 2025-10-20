d1e0d285a434864e0d896b3c583a29b41b275041
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-10-20 03:37:19 -0600

    feat: complete header

--- include/header.php
+++ include/header.php
@@ -5,9 +5,9 @@ function render_header(): void
         <h2><a href="index.php">PORTFOLIO PROJECT</a></h2>
         <nav>
             <ul>
-                <li><a href="company.php">HOME</a></li>
-                <li><a>ABOUT</a></li>
-                <li><a>API TESTER</a></li>
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="about.php">ABOUT</a></li>
+                <li><a href="about.php">API TESTER</a></li>
             </ul>
         </nav>
     </header>
